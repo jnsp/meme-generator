080dd946e9edc9f6d53a189d4b5a632c766321d8
Jangsea Park <jangsea.park@gmail.com>
2021-02-05 21:51:58 +0000

    Fix make_meme() return output path

--- meme_engine/meme_generator.py
+++ meme_engine/meme_generator.py
@@ -7,7 +7,8 @@ class MemeGenerator:
     def __init__(self, output_dir: str) -> None:
         self._output_dir = output_dir
 
-    def make_meme(self, img_path, text, author, width=500) -> str:
+    def make_meme(self, img_path: str, text: str, author: str,
+                  width: int = 500, meme_file_name: str = None) -> str:
         with Image.open(img_path) as im:
             img_widht, img_height = im.size
             height = int(width * img_height / img_widht)
@@ -20,5 +21,16 @@ class MemeGenerator:
             draw.text(position, quote, font=font, fill='black', stroke_width=2,
                       stroke_fill='white')
 
-            im.save(self._output_dir)
-        return self._output_dir
+            output_path = self._make_output_path(img_path, meme_file_name)
+            im.save(output_path)
+
+        return output_path
+
+    def _make_output_path(self, img_path, meme_file_name):
+        if meme_file_name is None:
+            num = 1000000
+            random_name = str(randint(0, num)).zfill(len(str(num)))
+            meme_file_name = f'meme_{random_name}'
+
+        ext = img_path.split('.')[-1]
+        return f'{self._output_dir}/{meme_file_name}.{ext}'
--- tests/test_meme_engine.py
+++ tests/test_meme_engine.py
@@ -1,6 +1,8 @@
 import os
+import re
 
 from PIL import Image
+import pytest
 
 from meme_engine import MemeGenerator
 
@@ -13,33 +15,56 @@ def test_meme_generator():
 
 def test_image_resize():
     img_path = './_data/photos/resize_test.jpg'
-    output_dir = './_data/photos/resized.jpg'
-    meme_gen = MemeGenerator(output_dir)
+    output_dir = './tmp'
+    meme_name = 'test_meme'
 
+    meme_gen = MemeGenerator(output_dir)
+    expected = f'{output_dir}/{meme_name}.jpg'
     try:
-        assert meme_gen.make_meme(img_path, 'TEXT', 'AUTHOR') == output_dir
-        with Image.open(output_dir) as im:
+        assert meme_gen.make_meme(img_path, 'TEXT', 'AUTHOR',
+                                  meme_file_name=meme_name) == expected
+        with Image.open(expected) as im:
             assert im.size[0] == 500
 
         target_width = 600
         assert meme_gen.make_meme(img_path, 'TEXT', 'AUTHOR',
-                                  target_width) == output_dir
-        with Image.open(output_dir) as im:
+                                  width=target_width,
+                                  meme_file_name=meme_name) == expected
+        with Image.open(expected) as im:
             assert im.size[0] == target_width
 
     finally:
-        os.remove(output_dir)
+        os.remove(expected)
+
+
+@pytest.mark.parametrize('ext', ['jpg', 'png', 'any_ext'])
+def test_make_output_path(ext):
+    output_dir = 'TEST_DIR'
+    img_path = f'./TEST_DIR/TEST_IMAGE.{ext}'
+    meme_gen = MemeGenerator(output_dir)
+
+    actual = meme_gen._make_output_path(img_path, None)
+    assert re.match(fr'TEST_DIR\/meme_[0-9]{{7}}\.{ext}', actual)
+
+    meme_name = 'TEST_MEME_NAME'
+    actual = meme_gen._make_output_path(img_path, meme_name)
+    assert actual == f'TEST_DIR/TEST_MEME_NAME.{ext}'
 
 
 def test_meme_generator_make_meme():
-    output_dir = './_data/photos/dog/example_output.jpg'
+    output_dir = './tmp'
     img_path = './_data/photos/dog/xander_1.jpg'
-    text = 'TEST_QUATE_BODY'
+    text = 'TEST_QUOTE_BODY'
     author = 'Tester'
+    meme_name = 'TEST_MEME_NAME'
+
     meme_gen = MemeGenerator(output_dir)
+    expected = f'{output_dir}/{meme_name}.jpg'
 
     try:
-        assert meme_gen.make_meme(img_path, text, author) == output_dir
-        assert os.path.exists(output_dir)
+        actual = meme_gen.make_meme(img_path, text, author,
+                                    meme_file_name=meme_name)
+        assert actual == expected
+        assert os.path.exists(expected)
     finally:
-        os.remove(output_dir)
+        os.remove(expected)
